a4c1517ab2c97517edf77b2443d8da2ee2869e66
Simon Bordewisch <sbordewisch@gmx.de>
2020-02-12 12:57:04 +0100

    Change content to hashmap instead of String & import Vertex

--- src/main/java/streaming/Edge.java
+++ src/main/java/streaming/Edge.java
@@ -1,27 +1,52 @@
 package streaming;
 
+import java.util.HashMap;
+
 public class Edge {
 
-    private String from;
-    private String to;
-    private String content;
+    private Vertex from;
+    private Vertex to;
+    private HashMap<String, String> content;
+
+    public Edge() {
+        this.from = new Vertex();
+        this.to = new Vertex();
+        this.content = new HashMap<>();
+    }
 
 
-    public Edge(String from, String to, String content) {
+    public Edge(Vertex from, Vertex to, HashMap<String,String> content) {
         this.from = from;
         this.to = to;
         this.content = content;
     }
 
-    public String getFrom() {
+    public Edge(String fromStr, String toStr, String contentStr){
+        this();
+        from.put("id", fromStr);
+        to.put("id", toStr);
+        content.put("id", contentStr);
+    }
+
+
+    public String get(String key) {
+        return content.get(key);
+    }
+
+    public void put(String key, String value) {
+        content.put(key, value);
+    }
+
+
+    public Vertex getFrom() {
         return from;
     }
 
-    public String getTo() {
+    public Vertex getTo() {
         return to;
     }
 
-    public String getContent() {
+    public HashMap<String, String> getContent() {
         return content;
     }
 
