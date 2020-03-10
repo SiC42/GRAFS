ce43f99fe1340fbbc9bc815d38bbb43140fd17aa
Simon Bordewisch <sbordewisch@gmx.de>
2020-03-10 14:46:02 +0100

    Add ascii-loader for making graphs out of strings

---
+++ src/main/java/streaming/helper/AsciiGraphLoader.java
@@ -0,0 +1,29 @@
+package streaming.helper;
+
+import org.s1ck.gdl.GDLHandler;
+import org.s1ck.gdl.model.Vertex;
+import streaming.model.Edge;
+
+import java.util.*;
+
+public class AsciiGraphLoader {
+
+    public static Collection<Edge> loadFromString(String graphStr) {
+        GDLHandler handler = new GDLHandler.Builder().buildFromString(graphStr);
+        Set<Edge> edges = new HashSet<>();
+        Map<Long, Vertex> verticesDic = new HashMap<>();
+        for(Vertex v : handler.getVertices()) {
+            verticesDic.put(v.getId(),v);
+        }
+        for(org.s1ck.gdl.model.Edge gdlE : handler.getEdges()){
+            long source = gdlE.getSourceVertexId();
+            Vertex sourceV = verticesDic.get(source);
+            long target = gdlE.getTargetVertexId();
+            Vertex targetV = verticesDic.get(target);
+            System.out.println(targetV.getVariable() + ": " + targetV.getGraphs());
+            Edge e = new streaming.model.Edge(gdlE,sourceV,targetV);
+            edges.add(e);
+        }
+        return edges;
+    }
+}
