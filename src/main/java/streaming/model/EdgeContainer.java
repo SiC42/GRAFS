23d1884ba346ae4c4fa962c1cd3ed03715bdb35d
Simon Bordewisch <sbordewisch@gmx.de>
2020-05-28 13:37:56 +0200

    Add new constructors

--- src/main/java/streaming/model/EdgeContainer.java
+++ src/main/java/streaming/model/EdgeContainer.java
@@ -13,6 +13,12 @@ public class EdgeContainer {
     this.targetVertex = targetVertex;
   }
 
+  public EdgeContainer(Element edgeElement, Element sourceVertex, Element targetVertex){
+    this.sourceVertex = new Vertex(sourceVertex);
+    this.targetVertex = new Vertex(targetVertex);
+    this.edge = new Edge(edgeElement, sourceVertex.getId(), targetVertex.getId());
+  }
+
   public Edge getEdge() {
     return edge;
   }
