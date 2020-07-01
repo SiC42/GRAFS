610678391538c017875b2d8a60af83b006c1cd0b
Simon Bordewisch <sbordewisch@gmx.de>
2020-07-01 13:05:20 +0200

    Update SubgraphTest.java

--- src/test/java/streaming/operators/subgraph/SubgraphTest.java
+++ src/test/java/streaming/operators/subgraph/SubgraphTest.java
@@ -12,6 +12,7 @@ import streaming.operators.OperatorTestBase;
 import streaming.util.AsciiGraphLoader;
 
 public class SubgraphTest extends OperatorTestBase {
+
   @Test
   public void testExistingSubgraph() throws Exception {
     AsciiGraphLoader loader = getSocialNetworkLoader();
@@ -41,3 +42,108 @@ public class SubgraphTest extends OperatorTestBase {
     }
     assertEquals(expected, actual);
   }
+
+  /**
+   * Extracts a subgraph where only vertices fulfill the filter function.
+   */
+  @Test
+  public void testPartialSubgraph() throws Exception {
+    AsciiGraphLoader loader = getSocialNetworkLoader();
+
+    loader.appendFromString("expected:_DB[" +
+        "(alice),(bob),(carol),(dave),(eve),(frank)" +
+        "]");
+
+    EdgeStream input = loader.createEdgeStream(getConfig());
+
+    Collection<EdgeContainer> expected =
+        loader.createEdgeContainersByGraphVariables("expected");
+
+    Iterator<EdgeContainer> output = input
+        .subgraph(
+            v -> v.getLabel().equals("Person"),
+            e -> e.getLabel().equals("friendOf")).collect();
+
+    Collection<EdgeContainer> actual = new HashSet<>();
+    while (output.hasNext()) {
+      actual.add(output.next());
+    }
+    assertEquals(expected, actual);
+  }
+
+  /**
+   * Extracts a subgraph which is empty.
+   *
+   * @throws Exception on failure
+   */
+  @Test
+  public void testEmptySubgraph() throws Exception {
+    AsciiGraphLoader loader = getSocialNetworkLoader();
+
+    loader.appendFromString("expected:_DB[]");
+
+    EdgeStream input = loader.createEdgeStream(getConfig());
+
+    Collection<EdgeContainer> expected =
+        loader.createEdgeContainersByGraphVariables("expected");
+
+    Iterator<EdgeContainer> output = input.subgraph(
+        v -> v.getLabel().equals("User"),
+        e -> e.getLabel().equals("friendOf")).collect();
+
+    Collection<EdgeContainer> actual = new HashSet<>();
+    while (output.hasNext()) {
+      actual.add(output.next());
+    }
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testVertexInducedSubgraph() throws Exception {
+    AsciiGraphLoader loader = getSocialNetworkLoader();
+
+    loader.appendFromString("expected:_DB[" +
+        "(databases)<-[ghtd]-(gdbs)-[ghtg1]->(graphs)" +
+        "(graphs)<-[ghtg2]-(gps)-[ghth]->(hadoop)" +
+        "]");
+
+    EdgeStream input = loader.createEdgeStream(getConfig());
+
+    Collection<EdgeContainer> expected =
+        loader.createEdgeContainersByGraphVariables("expected");
+
+    Iterator<EdgeContainer> output = input.vertexInducedSubgraph(
+        v -> v.getLabel().equals("Forum") || v.getLabel().equals("Tag")).collect();
+
+    Collection<EdgeContainer> actual = new HashSet<>();
+    while (output.hasNext()) {
+      actual.add(output.next());
+    }
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testEdgeInducedSubgraph() throws Exception {
+    AsciiGraphLoader loader = getSocialNetworkLoader();
+
+    loader.appendFromString("expected:_DB[" +
+        "(databases)<-[ghtd]-(gdbs)-[ghtg1]->(graphs)" +
+        "(graphs)<-[ghtg2]-(gps)-[ghth]->(hadoop)" +
+        "]");
+
+    EdgeStream input = loader.createEdgeStream(getConfig());
+
+    Collection<EdgeContainer> expected =
+        loader.createEdgeContainersByGraphVariables("expected");
+
+    Iterator<EdgeContainer> output = input.edgeInducedSubgraph(
+        e -> e.getLabel().equals("hasTag")).collect();
+    Collection<EdgeContainer> actual = new HashSet<>();
+    while (output.hasNext()) {
+      actual.add(output.next());
+    }
+    assertEquals(expected, actual);
+  }
+
+
+}
