35f91267ec727151b9764838a1a5f91883c401c7
Simon Bordewisch <sbordewisch@gmx.de>
2020-07-01 13:12:03 +0200

    Refactor operator interface
    we dont need generic operator interface for now

--- src/main/java/streaming/operators/OperatorI.java
+++ src/main/java/streaming/operators/OperatorI.java
@@ -1,10 +1,10 @@
 package streaming.operators;
 
 import org.apache.flink.streaming.api.datastream.DataStream;
-import streaming.model.Edge;
 import streaming.model.EdgeContainer;
-import streaming.model.EdgeStream;
 
-public interface OperatorI extends GenericOperatorI<EdgeContainer, EdgeContainer> {
+public interface OperatorI {
+
+  DataStream<EdgeContainer> execute(DataStream<EdgeContainer> stream);
 
 }
