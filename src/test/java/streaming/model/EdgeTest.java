f53bdb4515eb66b39098e8943902366318ee2f4c
Simon Bordewisch <sbordewisch@gmx.de>
2020-07-01 13:59:44 +0200

    Add tests for reverse edge

--- src/test/java/streaming/model/EdgeTest.java
+++ src/test/java/streaming/model/EdgeTest.java
@@ -103,7 +103,18 @@ public class EdgeTest {
     assertThrows(NullPointerException.class, () -> {
       new EdgeFactory().initEdge(edgeId, null, sourceId, targetId);
     });
+  }
 
+  @Test
+  public void createInvertedEdgeTest() {
+    GradoopId edgeId = GradoopId.get();
+    GradoopId sourceId = GradoopId.get();
+    GradoopId targetId = GradoopId.get();
+    Edge e = new EdgeFactory().initEdge(edgeId, sourceId, targetId);
+    Edge invertedE = e.createReverseEdge();
+    assertEquals(invertedE.getSourceId(), invertedE.getSourceId());
+    assertEquals(invertedE.getTargetId(), invertedE.getTargetId());
+    assertEquals(invertedE, invertedE);
   }
 }
 
