9752d6f80d50683aba9b562aca5c96a2e9a18a10
Simon Bordewisch <sbordewisch@gmx.de>
2020-07-01 14:00:18 +0200

    Remodel transformation classes

--- src/main/java/streaming/operators/transform/EdgeTransformation.java
+++ src/main/java/streaming/operators/transform/EdgeTransformation.java
@@ -4,31 +4,21 @@ import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import streaming.model.Edge;
 import streaming.model.EdgeContainer;
-import streaming.model.GraphElement;
 import streaming.operators.OperatorI;
 
 public class EdgeTransformation implements OperatorI {
 
-  private final MapFunction<Edge, Edge> mapper;
+  private final MapFunction<EdgeContainer, EdgeContainer> ecMapper;
 
   public EdgeTransformation(final MapFunction<Edge, Edge> mapper) {
-    this.mapper = mapper;
-  }
-
-
-  @Override
-  public DataStream<EdgeContainer> execute(DataStream<EdgeContainer> stream) {
-    return transform(stream);
-  }
-
-  private DataStream<EdgeContainer> transform(DataStream<EdgeContainer> stream) {
-    // TODO: Make this save (can there be deepcopy-refs here?)
-    MapFunction<EdgeContainer, EdgeContainer> edgeContainerMapper = ec -> {
+    this.ecMapper = ec -> {
       mapper.map(ec.getEdge());
       return ec;
     };
+  }
 
-    DataStream<EdgeContainer> filteredStream = stream.map(edgeContainerMapper);
-    return filteredStream;
+  @Override
+  public DataStream<EdgeContainer> execute(DataStream<EdgeContainer> stream) {
+    return stream.map(ecMapper);
   }
 }
--- src/main/java/streaming/operators/transform/VertexTransformation.java
+++ src/main/java/streaming/operators/transform/VertexTransformation.java
@@ -2,32 +2,25 @@ package streaming.operators.transform;
 
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
-import streaming.model.Edge;
 import streaming.model.EdgeContainer;
 import streaming.model.Vertex;
 import streaming.operators.OperatorI;
 
 public class VertexTransformation implements OperatorI {
 
-  private final MapFunction<Vertex, Vertex> mapper;
+  private final MapFunction<EdgeContainer, EdgeContainer> ecMapper;
 
   public VertexTransformation(final MapFunction<Vertex, Vertex> mapper) {
-    this.mapper = mapper;
+    this.ecMapper = ec -> {
+      Vertex target = mapper.map(ec.getTargetVertex());
+      Vertex source = mapper.map(ec.getSourceVertex());
+      return new EdgeContainer(ec.getEdge(), target, source);
+    };
+    ;
   }
-
 
   @Override
   public DataStream<EdgeContainer> execute(DataStream<EdgeContainer> stream) {
-    return transform(stream);
-  }
-
-  public DataStream<EdgeContainer> transform(DataStream<EdgeContainer> stream) {
-    MapFunction<EdgeContainer, EdgeContainer> transformVerticesFunction =
-        ec -> {
-          Vertex target = mapper.map(ec.getTargetVertex());
-          Vertex source = mapper.map(ec.getSourceVertex());
-          return new EdgeContainer(ec.getEdge(), target, source);
-        };
-    return stream.map(transformVerticesFunction);
+    return stream.map(ecMapper);
   }
 }
