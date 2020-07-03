340ab63510321a8ebed30ea4b476cd2c3736ad45
Simon Bordewisch <sbordewisch@gmx.de>
2020-07-03 14:55:13 +0200

    Split test in base test class and class specific tests

--- src/test/java/streaming/operators/transform/EdgeTransformationTest.java
+++ src/test/java/streaming/operators/transform/EdgeTransformationTest.java
@@ -1,30 +1,24 @@
 package streaming.operators.transform;
 
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static streaming.util.TestUtils.validateElementCollections;
 import static streaming.util.TestUtils.validateIdEquality;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Comparator;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.gradoop.common.model.impl.id.GradoopId;
-import org.gradoop.common.model.impl.properties.Properties;
 import org.junit.jupiter.api.Test;
 import streaming.model.Edge;
-import streaming.model.EdgeContainer;
 import streaming.model.EdgeStream;
 import streaming.model.Element;
 import streaming.model.Vertex;
-import streaming.operators.OperatorTestBase;
 import streaming.util.AsciiGraphLoader;
 
-public class EdgeTransformationTest extends OperatorTestBase {
+public class EdgeTransformationTest extends TransformationBaseTest {
 
   final String testGraphString = "" +
       "g0:A  { a : 1 } [(:A { a : 1, b : 2 })-[:a { a : 1, b : 2 }]->(:B { c : 2 })]" +
@@ -40,36 +34,13 @@ public class EdgeTransformationTest extends OperatorTestBase {
     return current;
   }
 
-  public static void validateElementsDifference(Element originalElement,
-      Element actualElement) {
-
-    Properties originalProperties = Properties.create();
-    originalElement.getProperties().forEach(originalProperties::set);
-    Properties actualProperties = Properties.create();
-    actualElement.getProperties().forEach(actualProperties::set);
-
-    for (var originalProp : originalProperties) {
-      var originalKey = originalProp.getKey();
-      if (actualProperties.containsKey(originalKey)) {
-        assertNotEquals(originalProp, actualProperties.get(originalKey));
-        actualProperties.remove(originalKey);
-      }
-    }
-    for (var actualProp : actualProperties) {
-      var actualKey = actualProp.getKey();
-      if (actualProperties.containsKey(actualKey)) {
-        assertNotEquals(originalProperties.get(actualKey), actualProp);
-      }
-    }
-  }
-
   @Test
   public void testIdEquality() throws Exception {
     AsciiGraphLoader loader = getLoaderFromString(testGraphString);
 
-    var referenceCollection = loader.createEdgeContainersByGraphVariables("g0");
-    List<GradoopId> expectedEdgeIds = referenceCollection.stream()
-        .map(ec -> ec.getEdge().getId())
+    Collection<Edge> referenceEdges = loader.getEdgesByGraphVariables("g0");
+    List<GradoopId> expectedEdgeIds = referenceEdges.stream()
+        .map(Element::getId)
         .collect(Collectors.toCollection(ArrayList::new));
 
     var inputStream = loader.createEdgeStreamByGraphVariables(getConfig(), "g0");
@@ -89,10 +60,7 @@ public class EdgeTransformationTest extends OperatorTestBase {
   public void testDataInequality() throws Exception {
     AsciiGraphLoader loader = getLoaderFromString(testGraphString);
 
-    var referenceCollection = loader.createEdgeContainersByGraphVariables("g0");
-    List<Edge> referenceEdges = referenceCollection.stream()
-        .map(EdgeContainer::getEdge)
-        .collect(Collectors.toList());
+    Collection<Edge> referenceEdges = loader.getEdgesByGraphVariables("g01");
 
     var inputStream = loader.createEdgeStreamByGraphVariables(getConfig(), "g0");
     var ecResultIterator = inputStream
@@ -110,49 +78,16 @@ public class EdgeTransformationTest extends OperatorTestBase {
   }
 
   /**
-   * Checks if two collections contain the same EPGM elements in terms of data (i.e. label and
-   * properties).
-   *
-   * @param expectedCollection first collection
-   * @param actualCollection   second collection
-   */
-  private void validateDataInequality(Collection<? extends Element> expectedCollection,
-      Collection<? extends Element> actualCollection) {
-
-    List<? extends Element> list1 = new ArrayList<>(expectedCollection);
-    List<? extends Element> list2 = new ArrayList<>(actualCollection);
-    final Comparator<Element> ID_COMPARATOR = Comparator.comparing(Element::getId);
-    list1.sort(ID_COMPARATOR);
-    list2.sort(ID_COMPARATOR);
-
-    Iterator<? extends Element> it1 = list1.iterator();
-    Iterator<? extends Element> it2 = list2.iterator();
-
-    while (it1.hasNext()) {
-      validateElementsDifference(
-          it1.next(),
-          it2.next());
-    }
-  }
-
-  /**
    * Tests the data in the resulting graph.
    *
    * @throws Exception on failure
    */
   @Test
-  public void testEdgeOnlyTransformation() throws Exception {
+  public void testEdgeTransformation() throws Exception {
     AsciiGraphLoader loader = getLoaderFromString(testGraphString);
 
-    var expectedEcList = loader.createEdgeContainersByGraphVariables("g01");
-    Set<Edge> expectedEdgeResult = expectedEcList.stream()
-        .map(EdgeContainer::getEdge)
-        .collect(Collectors.toSet());
-    Set<Vertex> expectedVertexResult = new HashSet<>();
-    expectedEcList.forEach(ec -> {
-      expectedVertexResult.add(ec.getSourceVertex());
-      expectedVertexResult.add(ec.getTargetVertex());
-    });
+    Collection<Edge> expectedEdges = loader.getEdgesByGraphVariables("g01");
+    Collection<Vertex> expectedVertices = loader.getVerticesByGraphVariables("g01");
 
     EdgeStream original = loader.createEdgeStreamByGraphVariables(getConfig(), "g0");
 
@@ -167,8 +102,8 @@ public class EdgeTransformationTest extends OperatorTestBase {
       actualVertexResult.add(ec.getTargetVertex());
     }
 
-    validateElementCollections(expectedEdgeResult, actualEdgeResult);
-    validateElementCollections(expectedVertexResult, actualVertexResult);
+    validateElementCollections(expectedEdges, actualEdgeResult);
+    validateElementCollections(expectedVertices, actualVertexResult);
   }
 
 
