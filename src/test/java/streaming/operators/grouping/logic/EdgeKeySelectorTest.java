628bda2f4d16e11b614f3ce02eeebe1d50147b0f
Simon Bordewisch <sbordewisch@gmx.de>
2020-07-08 11:39:00 +0200

    Update EdgeKeySelectorTest.java

--- src/test/java/streaming/operators/grouping/logic/EdgeKeySelectorTest.java
+++ src/test/java/streaming/operators/grouping/logic/EdgeKeySelectorTest.java
@@ -3,8 +3,7 @@ package streaming.operators.grouping.logic;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Collection;
 import org.junit.jupiter.api.Test;
 import streaming.model.EdgeContainer;
 import streaming.operators.grouping.model.AggregateMode;
@@ -13,27 +12,67 @@ import streaming.util.AsciiGraphLoader;
 
 class EdgeKeySelectorTest {
 
-  @Test
-  void getKey() {
-    GroupingInformation egi = new GroupingInformation();
-    egi.groupingKeys.add("n");
+  private EdgeContainer ec;
+
+  public EdgeKeySelectorTest() {
     AsciiGraphLoader loader = AsciiGraphLoader.fromString(
-        "(a18 {n : \"A\", a : \"18\"})," +
-            "(a20 {n : \"A\", a : \"20\"})," +
-            "(a25 {n : \"A\", a : \"25\"})," +
-            "(b17 {n : \"B\", a : \"17\"})," +
-            "(b19 {n : \"B\", a : \"19\"})," +
-            "(c20 {n : \"C\", a : \"20\"})," +
-            "(a18)-[]->(b17)," +
-            "(a18)-[]->(c20)," +
-            "(c20)-[]->(a25)," +
-            "(c20)-[]->(b17)," +
-            "(a20)-[]->(b19),"
+        "(a18:v {n : \"A\", a : 18})," +
+            "(b17:v {n : \"B\", a : 17})," +
+            "(a18)-[e1:e {t: 5}]->(b17)"
     );
-    Set<EdgeContainer> edgeSet = new HashSet<>(loader.createEdgeContainers());
-    EdgeKeySelector eks = new EdgeKeySelector(egi, null, AggregateMode.SOURCE);
-    for (EdgeContainer e : edgeSet) {
-      assertThat(eks.getKey(e), equalTo("Vertex-Grouping-Information:(properties:{(n:A) })"));
-    }
+    Collection<EdgeContainer> edgeSet = loader.createEdgeContainers();
+    ec = edgeSet.iterator().next();
+
+  }
+
+  @Test
+  void getKey_forSourceVertex() {
+    GroupingInformation gi = new GroupingInformation();
+    gi.addKey("n");
+    EdgeKeySelector eks = new EdgeKeySelector(gi, null, AggregateMode.SOURCE);
+    assertThat(eks.getKey(ec), equalTo("({n:A})-[]->()"));
+  }
+
+  @Test
+  void getKey_testNumberProperty() {
+    GroupingInformation gi = new GroupingInformation();
+    gi.addKey("a");
+    EdgeKeySelector eks = new EdgeKeySelector(gi, null, AggregateMode.SOURCE);
+    assertThat(eks.getKey(ec), equalTo("({a:18})-[]->()"));
+  }
+
+  @Test
+  void getKey_testLabelForVertex() {
+    GroupingInformation gi = new GroupingInformation();
+    gi.useLabel(true);
+    EdgeKeySelector eks = new EdgeKeySelector(gi, null, AggregateMode.SOURCE);
+    assertThat(eks.getKey(ec), equalTo("(:v)-[]->()"));
+  }
+
+  @Test
+  void getKey_forTargetVertex() {
+    GroupingInformation gi = new GroupingInformation();
+    gi.addKey("n");
+    EdgeKeySelector eks = new EdgeKeySelector(gi, null, AggregateMode.TARGET);
+    assertThat(eks.getKey(ec), equalTo("()-[]->({n:B})"));
+  }
+
+  @Test
+  void getKey_forEdgeWithVertexGroupingInformation() {
+    GroupingInformation vgi = new GroupingInformation();
+    vgi.addKey("n");
+    GroupingInformation egi = new GroupingInformation();
+    egi.addKey("t");
+    EdgeKeySelector eks = new EdgeKeySelector(vgi, egi, AggregateMode.EDGE);
+    assertThat(eks.getKey(ec), equalTo("({n:A})-[{t:5}]->({n:B})"));
+  }
+
+  @Test
+  void getKey_testLabelForEdge() {
+    GroupingInformation vgi = new GroupingInformation();
+    GroupingInformation egi = new GroupingInformation();
+    egi.useLabel(true);
+    EdgeKeySelector eks = new EdgeKeySelector(vgi, egi, AggregateMode.EDGE);
+    assertThat(eks.getKey(ec), equalTo("()-[:e]->()"));
   }
 }
