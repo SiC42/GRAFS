2a79d2516fc38526464d9336c1d86099426f6818
Simon Bordewisch <sbordewisch@gmx.de>
2020-07-08 11:38:54 +0200

    Rewrite edgekeyselector to be represent a GDL-"blueprint"

--- src/main/java/streaming/operators/grouping/logic/EdgeKeySelector.java
+++ src/main/java/streaming/operators/grouping/logic/EdgeKeySelector.java
@@ -1,81 +1,94 @@
 package streaming.operators.grouping.logic;
 
+import java.util.Objects;
 import org.apache.flink.api.java.functions.KeySelector;
+import streaming.model.Edge;
 import streaming.model.EdgeContainer;
 import streaming.model.Element;
+import streaming.model.Vertex;
 import streaming.operators.grouping.model.AggregateMode;
 import streaming.operators.grouping.model.GroupingInformation;
 
 public class EdgeKeySelector implements KeySelector<EdgeContainer, String> {
 
-  private final GroupingInformation vertexEgi;
-  private final GroupingInformation edgeEgi;
+  private final GroupingInformation vertexGi;
+  private final GroupingInformation edgeGi;
   private final AggregateMode makeKeyFor;
 
-  public EdgeKeySelector(GroupingInformation vertexEgi, GroupingInformation edgeEgi,
+  public EdgeKeySelector(GroupingInformation vertexGi, GroupingInformation edgeGi,
       AggregateMode makeKeyFor) {
-    this.vertexEgi = vertexEgi;
-    this.edgeEgi = edgeEgi;
+    Objects.requireNonNull(vertexGi, "grouping information for vertex was null");
+    if (makeKeyFor.equals(AggregateMode.EDGE)) {
+      Objects.requireNonNull(edgeGi, "grouping information for edge was null");
+    }
+    this.vertexGi = vertexGi;
+    this.edgeGi = edgeGi;
     this.makeKeyFor = makeKeyFor;
   }
 
   @Override
   public String getKey(EdgeContainer ec) {
+    final String EMPTY_VERTEX = "()";
+    final String EMPTY_EDGE = "[]";
     switch (makeKeyFor) {
-
-      case SOURCE:
-        return generateKeyForSingleVertex(ec.getSourceVertex(), vertexEgi);
-      case TARGET:
-        return generateKeyForSingleVertex(ec.getTargetVertex(), vertexEgi);
-      case EDGE:
-        return generateKeyForEdge(ec, vertexEgi, edgeEgi);
+      case SOURCE: {
+        String vertex = generateKeyForVertex(ec.getSourceVertex(), vertexGi);
+        return generateKey(vertex, EMPTY_EDGE, EMPTY_VERTEX);
+      }
+      case TARGET: {
+        String vertex = generateKeyForVertex(ec.getTargetVertex(), vertexGi);
+        return generateKey(EMPTY_VERTEX, EMPTY_EDGE, vertex);
+      }
+      case EDGE: {
+        String source = generateKeyForVertex(ec.getSourceVertex(), vertexGi);
+        String target = generateKeyForVertex(ec.getTargetVertex(), vertexGi);
+        String edge = generateKeyForEdge(ec.getEdge(), edgeGi);
+        return generateKey(source, edge, target);
+      }
+      default:
+        throw new IllegalArgumentException("aggregate mode couldn't be found");
     }
-    return null;
   }
 
+  private String generateKey(String source, String edge, String target) {
+    return String.format("%s-%s->%s", source, edge, target);
+  }
 
-  private String generateKeyForSingleVertex(Element vertexGei,
-      GroupingInformation vertexEgi) {
+  private String generateKeyForVertex(Vertex sourceVertex, GroupingInformation vertexGi) {
     StringBuilder sb = new StringBuilder();
 
-    // Build Key based on Vertex Information
-    sb = keyStringBuilder(sb, vertexGei, vertexEgi, "Vertex");
-
+    // Build Key based on vertex Information
+    sb.append("(");
+    sb = keyStringBuilder(sb, sourceVertex, vertexGi);
+    sb.append(")");
     return sb.toString();
   }
 
-  private String generateKeyForEdge(EdgeContainer edgeContainer, GroupingInformation vertexEgi,
-      GroupingInformation edgeEgi) {
+  private String generateKeyForEdge(Edge edge, GroupingInformation edgeGi) {
     StringBuilder sb = new StringBuilder();
 
-    // Build Key based on Source Vertex Information
-    sb = keyStringBuilder(sb, edgeContainer.getSourceVertex(), vertexEgi, "Source");
-
-    // Build Key based on Edge Information
-    if (edgeEgi != null) {
-      sb = keyStringBuilder(sb, edgeContainer.getEdge(), edgeEgi, "Edge");
-    }
-
-    // Build Key based on Target Vertex Information
-    sb = keyStringBuilder(sb, edgeContainer.getTargetVertex(), vertexEgi, "Target");
-
+    // Build Key based on vertex Information
+    sb.append("[");
+    sb = keyStringBuilder(sb, edge, edgeGi);
+    sb.append("]");
     return sb.toString();
   }
 
   private StringBuilder keyStringBuilder(StringBuilder sb, Element
-      gei, GroupingInformation egi, String elementStr) {
-    sb.append(elementStr).append("-Grouping-Information:(");
-      sb.append(String.format("label:%s ", gei.getLabel()));
+      gei, GroupingInformation egi) {
     if (egi.shouldUseLabel()) {
-    }
-    if (!egi.groupingKeys.isEmpty()) {
-      sb.append("properties:{");
-        sb.append(String.format("(%s:%s) ", key, gei.getPropertyValue(key)));
-      for (String key : egi.getKeys()) {
+      sb.append(String.format(":%s", gei.getLabel()));
+      if (!egi.getKeys().isEmpty()) {
+        sb.append(" ");
       }
-      sb.append("}");
     }
-    sb.append(")");
+    if (!egi.getKeys().isEmpty()) {
+      sb.append("{");
+      for (String key : egi.getKeys()) {
+        sb.append(String.format("%s:%s,", key, gei.getPropertyValue(key)));
+      }
+      sb.replace(sb.length() - 1, sb.length(), "}");
+    }
     return sb;
   }
 }
