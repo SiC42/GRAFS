3d5b5da431a54297ca5d3452d3101cfaa1e70c46
Simon Bordewisch <sbordewisch@gmx.de>
2020-07-08 12:45:29 +0200

    Create GroupingInformationTest.java

---
+++ src/test/java/streaming/operators/grouping/model/GroupingInformationTest.java
@@ -0,0 +1,77 @@
+package streaming.operators.grouping.model;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import streaming.util.TestUtils;
+
+public class GroupingInformationTest {
+
+  @Test
+  public void testGroupingInformation_emptyConstructor(){
+    var gi = new GroupingInformation();
+    assertFalse(gi.shouldUseLabel());
+    assertFalse(gi.shouldUseMembership());
+    assertThat(gi.getKeys(), empty());
+  }
+
+  @Test
+  public void testGroupingInformation_withKeySet(){
+    var groupingKeys = Set.of(TestUtils.KEY_1, TestUtils.KEY_2);
+    var gi = new GroupingInformation(groupingKeys);
+    assertFalse(gi.shouldUseLabel());
+    assertFalse(gi.shouldUseMembership());
+    assertThat(gi.getKeys(), is(equalTo(groupingKeys)));
+  }
+
+  @Test
+  public void testGroupingInformation_withValues(){
+    var useLabel = true;
+    var useMemberShip = true;
+    var groupingKeys = Set.of(TestUtils.KEY_1, TestUtils.KEY_2);
+    var gi = new GroupingInformation(useLabel, useMemberShip, groupingKeys);
+    assertThat(gi.shouldUseLabel(), is(equalTo(useLabel)));
+    assertThat(gi.shouldUseMembership(), is(equalTo(useMemberShip)));
+    assertThat(gi.getKeys(), is(equalTo(groupingKeys)));
+  }
+
+  @Test
+  public void testAddKey(){
+    var gi = new GroupingInformation();
+    gi.addKey("1");
+    assertThat(gi.getKeys(), is(equalTo(Set.of("1"))));
+  }
+
+  @Test
+  public void testAddKey_twoOfSameKey(){
+    var gi = new GroupingInformation();
+    gi.addKey("1");
+    gi.addKey("1");
+    assertThat(gi.getKeys(), is(equalTo(Set.of("1"))));
+  }
+
+  @Test
+  public void testShouldUseLabel(){
+    var gi = new GroupingInformation();
+    gi.useLabel(true);
+    assertTrue(gi.shouldUseLabel());
+    gi.useLabel(false);
+    assertFalse(gi.shouldUseLabel());
+  }
+
+  @Test
+  public void testShouldUseMembership(){
+    var gi = new GroupingInformation();
+    gi.useMembership(true);
+    assertTrue(gi.shouldUseMembership());
+    gi.useMembership(false);
+    assertFalse(gi.shouldUseMembership());
+  }
+
+}
