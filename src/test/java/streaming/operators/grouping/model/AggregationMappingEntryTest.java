5c172d7ba78c772d3a4639d5fb310aa78844c7c3
Simon Bordewisch <sbordewisch@gmx.de>
2020-07-08 12:45:25 +0200

    Create AggregationMappingEntryTest.java

---
+++ src/test/java/streaming/operators/grouping/model/AggregationMappingEntryTest.java
@@ -0,0 +1,23 @@
+package streaming.operators.grouping.model;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+import org.gradoop.common.model.impl.properties.PropertyValue;
+import org.junit.jupiter.api.Test;
+import streaming.util.TestUtils;
+
+public class AggregationMappingEntryTest {
+
+  @Test
+  public void AggregationMappingEntry(){
+    var pv = new PropertyValue();
+        pv.setString(TestUtils.STRING_VAL_6);
+    var paf = new PropertiesAggregationFunction(pv, (v1, v2) -> v2);
+    var ame = new AggregationMappingEntry(TestUtils.KEY_0, paf);
+    assertThat(ame.getPropertyKey(), is(equalTo(TestUtils.KEY_0)));
+    assertThat(ame.getAggregationFunction(), is(equalTo(paf)));
+
+  }
+}
