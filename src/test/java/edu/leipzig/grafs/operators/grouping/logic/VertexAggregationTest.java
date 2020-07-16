9299542979f3cb59be0670ef7662fb024a71ed6a
Simon Bordewisch <sbordewisch@gmx.de>
2020-07-16 12:50:11 +0200

    Build framework for test

--- src/test/java/edu/leipzig/grafs/operators/grouping/logic/VertexAggregationTest.java
+++ src/test/java/edu/leipzig/grafs/operators/grouping/logic/VertexAggregationTest.java
@@ -1,6 +1,7 @@
 package edu.leipzig.grafs.operators.grouping.logic;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 
 import edu.leipzig.grafs.model.Edge;
 import edu.leipzig.grafs.model.EdgeContainer;
@@ -9,43 +10,45 @@ import edu.leipzig.grafs.operators.grouping.model.AggregationMapping;
 import edu.leipzig.grafs.operators.grouping.model.GroupingInformation;
 import edu.leipzig.grafs.util.AsciiGraphLoader;
 import edu.leipzig.grafs.util.TestUtils;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import org.apache.flink.streaming.api.windowing.windows.Window;
 import org.apache.flink.util.Collector;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 class VertexAggregationTest {
 
   @Test
   void flatMap() {
-    GroupingInformation egi = new GroupingInformation();
-    egi.addKey("n");
-    AggregationMapping am = new AggregationMapping();
-    am.addAggregationForProperty("a", TestUtils.INT_ADD_FUNC);
+    var groupInfo = new GroupingInformation();
+    groupInfo.addKey("n");
+    var aggMap = new AggregationMapping();
+    aggMap.addAggregationForProperty("a", TestUtils.INT_ADD_FUNC);
 
-    VertexAggregation incrementer = new VertexAggregation(egi, am,
+    var vertexAggregation = new VertexAggregation<>(groupInfo, aggMap,
         AggregateMode.SOURCE);
 
-    Collector<Edge> collector = mock(Collector.class);
+    Collector<EdgeContainer> collector = mock(Collector.class);
     AsciiGraphLoader loader = AsciiGraphLoader.fromString(
-        "(a18 {n : \"A\", a : \"18\"})," +
-            "(a20 {n : \"A\", a : \"20\"})," +
-            "(a25 {n : \"A\", a : \"25\"})," +
-            "(b17 {n : \"B\", a : \"17\"})," +
-            "(b19 {n : \"B\", a : \"19\"})," +
-            "(c20 {n : \"C\", a : \"20\"})," +
+        "(a18 {n : \"A\", a : 18})," +
+            "(a20 {n : \"A\", a : 20})," +
+            "(b17 {n : \"B\", a : 17})," +
+            "(b19 {n : \"B\", a : 19})," +
+            "(c20 {n : \"C\", a : 20})," +
             "(a18)-[]->(b17)," +
             "(a18)-[]->(c20)," +
-            "(c20)-[]->(a25)," +
-            "(c20)-[]->(b17)," +
             "(a20)-[]->(b19),"
     );
-    Set<EdgeContainer> edgeSet = new HashSet<>(loader.createEdgeContainers());
+    var edgeList = new ArrayList<>(loader.createEdgeContainers());
 
-    // call the methods that you have implemented
-    //incrementer.flatMap(2L, collector);
+    vertexAggregation.process("", null, List.of(edgeList.get(0)), collector);
+
+
 
     //verify collector was called with the right output
-    //Mockito.verify(collector, times(1)).collect(3L);
+    Mockito.verify(collector, times(1)).collect(edgeList.get(0));
   }
 }
