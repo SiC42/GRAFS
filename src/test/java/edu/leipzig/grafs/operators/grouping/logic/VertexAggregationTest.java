2f470c3064054af78361c2664891b96ced56bbe6
Simon Bordewisch <sbordewisch@gmx.de>
2020-07-20 12:12:45 +0200

    Update VertexAggregationTest.java

--- src/test/java/edu/leipzig/grafs/operators/grouping/logic/VertexAggregationTest.java
+++ src/test/java/edu/leipzig/grafs/operators/grouping/logic/VertexAggregationTest.java
@@ -1,9 +1,7 @@
 package edu.leipzig.grafs.operators.grouping.logic;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
+import static edu.leipzig.grafs.util.TestUtils.validateEdgeContainerCollections;
 
-import edu.leipzig.grafs.model.Edge;
 import edu.leipzig.grafs.model.EdgeContainer;
 import edu.leipzig.grafs.operators.grouping.model.AggregateMode;
 import edu.leipzig.grafs.operators.grouping.model.AggregationMapping;
@@ -11,18 +9,12 @@ import edu.leipzig.grafs.operators.grouping.model.GroupingInformation;
 import edu.leipzig.grafs.util.AsciiGraphLoader;
 import edu.leipzig.grafs.util.TestUtils;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import org.apache.flink.streaming.api.windowing.windows.Window;
-import org.apache.flink.util.Collector;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
-class VertexAggregationTest {
+class VertexAggregationTest extends AggregationBaseTest {
 
   @Test
-  void flatMap() {
+  void flatMap_testSimpleAggregationForSource() {
     var groupInfo = new GroupingInformation();
     groupInfo.addKey("n");
     var aggMap = new AggregationMapping();
@@ -31,7 +23,94 @@ class VertexAggregationTest {
     var vertexAggregation = new VertexAggregation<>(groupInfo, aggMap,
         AggregateMode.SOURCE);
 
-    Collector<EdgeContainer> collector = mock(Collector.class);
+    CollectorDummy collector = new CollectorDummy();
+    AsciiGraphLoader loader = AsciiGraphLoader.fromString(
+        "(a18 {n : \"A\", a : 18})," +
+            "(a20 {n : \"A\", a : 20})," +
+            "(a25 {n : \"A\", a : 25})," +
+            "(b17 {n : \"B\", a : 17})," +
+            "(b19 {n : \"B\", a : 19})," +
+            "(c20 {n : \"C\", a : 20})," +
+            "(a18)-[]->(b17)," +
+            "(a18)-[]->(c20)," +
+            "(a20)-[]->(b19),"
+    );
+
+    var edgeList = new ArrayList<>(loader.createEdgeContainers());
+
+    vertexAggregation.process("", null, edgeList, collector);
+
+    // create expected output
+    AsciiGraphLoader expectedOutputLoader = AsciiGraphLoader.fromString(
+        "(a38 {n : \"A\", a : 38})," +
+            "(b17 {n : \"B\", a : 17})," +
+            "(b19 {n : \"B\", a : 19})," +
+            "(c20 {n : \"C\", a : 20})," +
+            "(a38)-[]->(b17)," +
+            "(a38)-[]->(c20)," +
+            "(a38)-[]->(b19),"
+    );
+
+    var expectedEcCol = expectedOutputLoader.createEdgeContainers();
+
+    //verify collector was called with the right output
+    validateEdgeContainerCollections(expectedEcCol, collector.getCollected());
+  }
+
+  @Test
+  void flatMap_testSimpleAggregationForTarget() {
+    var groupInfo = new GroupingInformation();
+    groupInfo.addKey("n");
+    var aggMap = new AggregationMapping();
+    aggMap.addAggregationForProperty("a", TestUtils.INT_ADD_FUNC);
+
+    var vertexAggregation = new VertexAggregation<>(groupInfo, aggMap,
+        AggregateMode.TARGET);
+
+    CollectorDummy collector = new CollectorDummy();
+    AsciiGraphLoader loader = AsciiGraphLoader.fromString(
+        "(a18 {n : \"A\", a : 18})," +
+            "(a20 {n : \"A\", a : 20})," +
+            "(b17 {n : \"B\", a : 17})," +
+            "(b19 {n : \"B\", a : 19})," +
+            "(c20 {n : \"C\", a : 20})," +
+            "(a18)-[]->(b17)," +
+            "(c20)-[]->(b17)," +
+            "(a20)-[]->(b19),"
+    );
+
+    var edgeList = new ArrayList<>(loader.createEdgeContainers());
+
+    vertexAggregation.process("", null, edgeList, collector);
+
+    // create expected output
+    AsciiGraphLoader expectedOutputLoader = AsciiGraphLoader.fromString(
+        "(a18 {n : \"A\", a : 18})," +
+            "(a20 {n : \"A\", a : 20})," +
+            "(b36 {n : \"B\", a : 36})," +
+            "(c20 {n : \"C\", a : 20})," +
+            "(a18)-[]->(b36)," +
+            "(c20)-[]->(b36)," +
+            "(a20)-[]->(b36),"
+    );
+
+    var expectedEcCol = expectedOutputLoader.createEdgeContainers();
+
+    //verify collector was called with the right output
+    validateEdgeContainerCollections(expectedEcCol, collector.getCollected());
+  }
+
+  @Test
+  void flatMap_testSimpleAggregationWithInvertedEdge() {
+    var groupInfo = new GroupingInformation();
+    groupInfo.addKey("n");
+    var aggMap = new AggregationMapping();
+    aggMap.addAggregationForProperty("a", TestUtils.INT_ADD_FUNC);
+
+    var vertexAggregation = new VertexAggregation<>(groupInfo, aggMap,
+        AggregateMode.SOURCE);
+
+    CollectorDummy collector = new CollectorDummy();
     AsciiGraphLoader loader = AsciiGraphLoader.fromString(
         "(a18 {n : \"A\", a : 18})," +
             "(a20 {n : \"A\", a : 20})," +
@@ -42,13 +121,47 @@ class VertexAggregationTest {
             "(a18)-[]->(c20)," +
             "(a20)-[]->(b19),"
     );
+
     var edgeList = new ArrayList<>(loader.createEdgeContainers());
+    loader = AsciiGraphLoader.fromString(
+        "(a25 {n : \"A\", a : 25})," +
+            "(c20 {n : \"C\", a : 20})," +
+            "(c20)-[]->(a25),"
+    );
 
-    vertexAggregation.process("", null, List.of(edgeList.get(0)), collector);
+    var invertedEdgeList = new ArrayList<>(loader.createEdgeContainers());
+    EdgeContainer invertedEc = invertedEdgeList.get(0);
+    invertedEc = invertedEc.createReverseEdgeContainer();
+    edgeList.add(invertedEc);
 
+    vertexAggregation.process("", null, edgeList, collector);
 
+    // create expected output
+    AsciiGraphLoader expectedOutputLoader = AsciiGraphLoader.fromString(
+        "(a63 {n : \"A\", a : 63})," +
+            "(b17 {n : \"B\", a : 17})," +
+            "(b19 {n : \"B\", a : 19})," +
+            "(c20 {n : \"C\", a : 20})," +
+            "(a63)-[]->(b17)," +
+            "(a63)-[]->(c20)," +
+            "(a63)-[]->(b19)"
+    );
+
+    var expectedEcCol = expectedOutputLoader.createEdgeContainers();
+
+    loader = AsciiGraphLoader.fromString(
+        "(a25 {n : \"A\", a : 25})," +
+            "(c20 {n : \"C\", a : 20})," +
+            "(c20)-[]->(a25)"
+    );
+
+    var expectedInvertedEdgeList = new ArrayList<>(loader.createEdgeContainers());
+    EdgeContainer expectedInvertedEc = expectedInvertedEdgeList.get(0);
+    expectedInvertedEc = expectedInvertedEc.createReverseEdgeContainer();
+    expectedEcCol.add(expectedInvertedEc);
 
     //verify collector was called with the right output
-    Mockito.verify(collector, times(1)).collect(edgeList.get(0));
+    validateEdgeContainerCollections(expectedEcCol, collector.getCollected());
   }
+
 }
