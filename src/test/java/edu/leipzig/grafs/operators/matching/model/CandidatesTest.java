8bf2f4bbba3dc009087b10752f850be2c45d1f68
Simon Bordewisch <sbordewisch@gmx.de>
2020-08-25 14:44:52 +0200

    Add tests for candidates

--- src/test/java/edu/leipzig/grafs/operators/matching/model/CandidatesTest.java
+++ src/test/java/edu/leipzig/grafs/operators/matching/model/CandidatesTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
+import java.util.Collections;
 import java.util.Set;
 import org.junit.jupiter.api.Test;
 
@@ -32,49 +33,91 @@ class CandidatesTest {
 
   @Test
   void testHasCandidate() {
+    var candidates = new Candidates<Integer>();
+    candidates.addCandidate(1, 2);
+    candidates.addCandidate(1, 3);
+    candidates.addCandidate(4, 2);
+    candidates.addCandidate(4, 6);
+    assertThat(candidates.hasCandidate(1, 2), is(true));
+    assertThat(candidates.hasCandidate(1, 3), is(true));
+    assertThat(candidates.hasCandidate(4, 2), is(true));
+    assertThat(candidates.hasCandidate(4, 6), is(true));
   }
 
   @Test
   void testDeleteCandidates() {
+    var candidates = new Candidates<Integer>();
+    candidates.addCandidates(1, Set.of(1, 2, 3));
+    assertThat(candidates.getCandidatesFor(1), is(equalTo(Set.of(1, 2, 3))));
+    assertThat(candidates.size(), is(3));
+    candidates.removeCandidates(1, Set.of(2, 3));
+    assertThat(candidates.getCandidatesFor(1), is(equalTo(Set.of(1))));
+    assertThat(candidates.size(), is(1));
   }
 
   @Test
   void testDeleteCandidate() {
+    var candidates = new Candidates<Integer>();
+    candidates.addCandidates(1, Set.of(1, 2));
+    assertThat(candidates.getCandidatesFor(1), is(equalTo(Set.of(1, 2))));
+    assertThat(candidates.size(), is(2));
+    candidates.removeCandidate(1, 2);
+    assertThat(candidates.getCandidatesFor(1), is(equalTo(Set.of(1))));
+    assertThat(candidates.size(), is(1));
   }
 
   @Test
-  void testAddCandidates() {
+  void testAddCandidates_withCandidateSet() {
+    var candidates = new Candidates<Integer>();
+    candidates.addCandidates(1, Set.of(1, 2));
+    assertThat(candidates.getCandidatesFor(1), is(equalTo(Set.of(1, 2))));
+    assertThat(candidates.size(), is(2));
+    candidates.addCandidates(1, Set.of(1, 3));
+    assertThat(candidates.getCandidatesFor(1), is(equalTo(Set.of(1, 2, 3))));
+    assertThat(candidates.size(), is(3));
   }
 
   @Test
-  void testTestAddCandidates() {
+  void testAddCandidates_withCandidatesObject() {
+    var candidates = new Candidates<Integer>();
+    candidates.addCandidates(1, Set.of(1, 2));
+
+    var candidates2 = new Candidates<Integer>();
+    candidates2.addCandidates(1, Set.of(1, 3));
+    candidates.addCandidates(candidates2);
+    assertThat(candidates.getCandidatesFor(1), is(equalTo(Set.of(1, 2, 3))));
+    assertThat(candidates.size(), is(3));
   }
 
   @Test
-  void testAsMultiMap() {
+  void testAddCandidates_withKeyOnly() {
+    var candidates = new Candidates<Integer>();
+    candidates.addCandidates(1, Set.of(1, 2, 3));
+    candidates.addCandidates(4, Set.of(5, 6, 7));
+    assertThat(candidates.size(), is(6));
+    candidates.removeCandidates(1);
+    assertThat(candidates.getCandidatesFor(4), is(equalTo(Set.of(5, 6, 7))));
+    assertThat(candidates.getCandidatesFor(1), is(equalTo(Collections.EMPTY_SET)));
+    assertThat(candidates.size(), is(3));
   }
 
-  @Test
-  void testIsEmpty() {
-  }
-
-  @Test
-  void testSize() {
-  }
 
   @Test
   void testRetainCandidates() {
-  }
-
-  @Test
-  void testRemoveAll() {
-  }
-
-  @Test
-  void testKeySet() {
+    var candidates = new Candidates<Integer>();
+    candidates.addCandidates(1, Set.of(1, 2, 3));
+    candidates.retainCandidates(1, Set.of(2, 4));
+    assertThat(candidates.getCandidatesFor(1), is(equalTo(Set.of(2))));
   }
 
   @Test
   void testIsCandidate() {
+    var candidates = new Candidates<Integer>();
+    candidates.addCandidates(4, Set.of(1, 2, 3));
+    candidates.addCandidates(5, Set.of(6, 7, 3));
+    for (var i : Set.of(1, 2, 3, 6, 7)) {
+      assertThat(candidates.isCandidate(i), is(true));
+    }
+    assertThat(candidates.isCandidate(4), is(false));
   }
 }
