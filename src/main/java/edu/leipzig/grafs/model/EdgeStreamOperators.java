d4fda847703b7b4139459f8c40beca005ee3ec57
Simon Bordewisch <sbordewisch@gmx.de>
2020-09-03 12:34:15 +0200

    Add sections via comments to EdgeStreamOperators

--- src/main/java/edu/leipzig/grafs/model/EdgeStreamOperators.java
+++ src/main/java/edu/leipzig/grafs/model/EdgeStreamOperators.java
@@ -1,6 +1,11 @@
 package edu.leipzig.grafs.model;
 
+import edu.leipzig.grafs.operators.grouping.Grouping;
+import edu.leipzig.grafs.operators.grouping.model.AggregationMapping;
+import edu.leipzig.grafs.operators.grouping.model.GroupingInformation;
 import edu.leipzig.grafs.operators.interfaces.OperatorI;
+import edu.leipzig.grafs.operators.matching.DualSimulation;
+import edu.leipzig.grafs.operators.matching.Isomorphism;
 import edu.leipzig.grafs.operators.subgraph.Subgraph;
 import edu.leipzig.grafs.operators.subgraph.Subgraph.Strategy;
 import edu.leipzig.grafs.operators.transform.EdgeTransformation;
@@ -10,14 +15,17 @@ import edu.leipzig.grafs.operators.union.UnionWithDuplicateInWindow;
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.windowing.assigners.WindowAssigner;
+import org.apache.flink.streaming.api.windowing.triggers.Trigger;
 import org.apache.flink.streaming.api.windowing.windows.Window;
 
 public interface EdgeStreamOperators {
 
   EdgeStream callForStream(OperatorI operator);
 
-  default EdgeStream vertexInducedSubgraph(
-      FilterFunction<Vertex> vertexGeiPredicate) {
+  // ---------------------------------------------------------------------------
+  //  Grouping operators
+  // ---------------------------------------------------------------------------
+
   default EdgeStream grouping(GroupingInformation vertexGi, AggregationMapping vertexAggMap,
       GroupingInformation edgeGi, AggregationMapping edgeAggMap,
       WindowAssigner<Object, Window> window) {
@@ -31,6 +39,11 @@ public interface EdgeStreamOperators {
     return callForStream(
         new Grouping<>(vertexGi, vertexAggMap, edgeGi, edgeAggMap, window, trigger));
   }
+
+  // ---------------------------------------------------------------------------
+  //  Matching operators
+  // ---------------------------------------------------------------------------
+
   default EdgeStream dualSimulation(String gdlQueryStr, WindowAssigner<Object, Window> window) {
     return callForStream(new DualSimulation<>(gdlQueryStr, window));
   }
@@ -39,6 +52,12 @@ public interface EdgeStreamOperators {
       WindowAssigner<Object, Window> window) {
     return callForStream(new Isomorphism<>(gdlQueryStr, window));
   }
+
+  // ---------------------------------------------------------------------------
+  //  Subgraph operators
+  // ---------------------------------------------------------------------------
+
+  default EdgeStream vertexInducedSubgraph(FilterFunction<Vertex> vertexGeiPredicate) {
     return callForStream(new Subgraph(vertexGeiPredicate, null, Strategy.VERTEX_INDUCED));
   }
 
@@ -56,6 +75,10 @@ public interface EdgeStreamOperators {
     return callForStream(new Subgraph(vertexGeiPredicate, edgeGeiPredicate, strategy));
   }
 
+  // ---------------------------------------------------------------------------
+  //  Transformation operators
+  // ---------------------------------------------------------------------------
+
   default EdgeStream transformEdges(MapFunction<Edge, Edge> mapper) {
     return callForStream(new EdgeTransformation(mapper));
   }
@@ -63,6 +86,10 @@ public interface EdgeStreamOperators {
   default EdgeStream transformVertices(MapFunction<Vertex, Vertex> mapper) {
     return callForStream(new VertexTransformation(mapper));
   }
+
+  // ---------------------------------------------------------------------------
+  //  Union operators
+  // ---------------------------------------------------------------------------
 
   /**
    * Union of two or more edge streams creating a new stream containing all the elements from all
