0a184e3eff83575a5e2ce01a68c9f2be39679b6d
Simon Bordewisch <sbordewisch@gmx.de>
2020-09-05 13:12:54 +0200

    Create BenchmarkDualSimulation.java

---
+++ src/main/java/edu/leipzig/grafs/benchmarking/matching/BenchmarkDualSimulation.java
@@ -0,0 +1,28 @@
+package edu.leipzig.grafs.benchmarking.matching;
+
+import edu.leipzig.grafs.benchmarking.functions.SimpleMeter;
+import edu.leipzig.grafs.model.EdgeContainer;
+import edu.leipzig.grafs.operators.matching.DualSimulation;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.windowing.assigners.WindowAssigner;
+import org.apache.flink.streaming.api.windowing.windows.Window;
+
+public class BenchmarkDualSimulation<W extends Window> extends DualSimulation<W> {
+
+
+  private final String meterName;
+
+  public BenchmarkDualSimulation(String query, WindowAssigner<Object, W> window) {
+    this(query, window, "dualSimulationMeter");
+  }
+  public BenchmarkDualSimulation(String query, WindowAssigner<Object, W> window, String meterName) {
+    super(query, window);
+
+    this.meterName = meterName;
+  }
+
+  @Override
+  public DataStream<EdgeContainer> execute(DataStream<EdgeContainer> stream) {
+    return super.execute(stream).map(new SimpleMeter<>(meterName));
+  }
+}
