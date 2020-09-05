196e76fccdca6a4fd1ec55dfeedc8c8b56dc4a17
Simon Bordewisch <sbordewisch@gmx.de>
2020-09-05 12:47:26 +0200

    Make general class for functions with meters

---
+++ src/main/java/edu/leipzig/grafs/benchmarking/generic/FunctionWithMeter.java
@@ -0,0 +1,23 @@
+package edu.leipzig.grafs.benchmarking.generic;
+
+import org.apache.flink.api.common.functions.AbstractRichFunction;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.dropwizard.metrics.DropwizardMeterWrapper;
+
+abstract class FunctionWithMeter extends AbstractRichFunction {
+
+  private final String name;
+
+  protected FunctionWithMeter(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public void open(Configuration parameters) throws Exception {
+    com.codahale.metrics.Meter dropwizardMeter = new com.codahale.metrics.Meter();
+    getRuntimeContext()
+        .getMetricGroup()
+        .meter(name, new DropwizardMeterWrapper(dropwizardMeter));
+  }
+
+}
