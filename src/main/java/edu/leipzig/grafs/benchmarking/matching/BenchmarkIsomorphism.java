d21dbc17fc343846cb44492bed16939fbe0dd4ce
Simon Bordewisch <sbordewisch@gmx.de>
2020-09-05 13:07:04 +0200

    Create BenchmarkIsomorphism.java

---
+++ src/main/java/edu/leipzig/grafs/benchmarking/matching/BenchmarkIsomorphism.java
@@ -0,0 +1,27 @@
+package edu.leipzig.grafs.benchmarking.matching;
+
+import edu.leipzig.grafs.benchmarking.generic.SimpleMeter;
+import edu.leipzig.grafs.model.EdgeContainer;
+import edu.leipzig.grafs.operators.matching.Isomorphism;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.windowing.assigners.WindowAssigner;
+import org.apache.flink.streaming.api.windowing.windows.Window;
+
+public class BenchmarkIsomorphism<W extends Window> extends Isomorphism<W> {
+
+  private final String meterName;
+
+  public BenchmarkIsomorphism(String query, WindowAssigner<Object, W> window) {
+    this(query, window, "isomorphismMeter");
+  }
+
+  public BenchmarkIsomorphism(String query, WindowAssigner<Object, W> window, String meterName) {
+    super(query, window);
+    this.meterName = meterName;
+  }
+
+  @Override
+  public DataStream<EdgeContainer> execute(DataStream<EdgeContainer> stream) {
+    return super.execute(stream).map(new SimpleMeter<>(meterName));
+  }
+}
