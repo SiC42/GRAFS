a6cc1a54ee809d496d7c88f7bf12fd5781fda68c
Simon Bordewisch <sbordewisch@gmx.de>
2020-09-05 12:48:45 +0200

    Make benchmarking class for subgraph

---
+++ src/main/java/edu/leipzig/grafs/benchmarking/subgraph/BenchmarkSubgraph.java
@@ -0,0 +1,32 @@
+package edu.leipzig.grafs.benchmarking.subgraph;
+
+import edu.leipzig.grafs.benchmarking.generic.FilterFunctionWithMeter;
+import edu.leipzig.grafs.model.Edge;
+import edu.leipzig.grafs.model.EdgeContainer;
+import edu.leipzig.grafs.model.Vertex;
+import edu.leipzig.grafs.operators.subgraph.Subgraph;
+import org.apache.flink.api.common.functions.FilterFunction;
+
+public class BenchmarkSubgraph extends Subgraph {
+
+  public BenchmarkSubgraph(
+      FilterFunction<Vertex> vertexFilter,
+      FilterFunction<Edge> edgeFilter,
+      Strategy strategy) {
+    this(vertexFilter, edgeFilter, strategy, "subgraphMeter");
+  }
+
+  public BenchmarkSubgraph(
+      FilterFunction<Vertex> vertexFilter,
+      FilterFunction<Edge> edgeFilter,
+      Strategy strategy,
+      String meterName) {
+    super(vertexFilter, edgeFilter, strategy);
+    this.ecFilter = new FilterFunctionWithMeter<EdgeContainer>(meterName) {
+      @Override
+      public boolean filter(EdgeContainer edgeContainer) throws Exception {
+        return ecFilter.filter(edgeContainer);
+      }
+    };
+  }
+}
