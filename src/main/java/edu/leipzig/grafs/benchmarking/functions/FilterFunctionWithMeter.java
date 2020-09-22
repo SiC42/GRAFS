6dd4a9744366524298be3be883df4b93a39d4e3a
Simon Bordewisch <sbordewisch@gmx.de>
2020-09-22 13:29:03 +0200

    Add meter and call its marker

--- src/main/java/edu/leipzig/grafs/benchmarking/functions/FilterFunctionWithMeter.java
+++ src/main/java/edu/leipzig/grafs/benchmarking/functions/FilterFunctionWithMeter.java
@@ -8,4 +8,12 @@ public abstract class FilterFunctionWithMeter<T> extends FunctionWithMeter imple
   protected FilterFunctionWithMeter(String name) {
     super(name);
   }
+
+  protected abstract boolean plainFilter(T t) throws Exception;
+
+  @Override
+  public boolean filter(T t) throws Exception {
+    this.meter.markEvent();
+    return plainFilter(t);
+  }
 }
--- src/main/java/edu/leipzig/grafs/benchmarking/functions/FunctionWithMeter.java
+++ src/main/java/edu/leipzig/grafs/benchmarking/functions/FunctionWithMeter.java
@@ -3,10 +3,13 @@ package edu.leipzig.grafs.benchmarking.functions;
 import org.apache.flink.api.common.functions.AbstractRichFunction;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.dropwizard.metrics.DropwizardMeterWrapper;
+import org.apache.flink.metrics.Meter;
 
 abstract class FunctionWithMeter extends AbstractRichFunction {
 
   private final String name;
+
+  protected transient Meter meter;
 
   protected FunctionWithMeter(String name) {
     this.name = name;
@@ -15,7 +18,7 @@ abstract class FunctionWithMeter extends AbstractRichFunction {
   @Override
   public void open(Configuration parameters) throws Exception {
     com.codahale.metrics.Meter dropwizardMeter = new com.codahale.metrics.Meter();
-    getRuntimeContext()
+    this.meter = getRuntimeContext()
         .getMetricGroup()
         .meter(name, new DropwizardMeterWrapper(dropwizardMeter));
   }
