b75291ff29e618ffd5c64e395b87929bd7790510
Simon Bordewisch <sbordewisch@gmx.de>
2020-09-25 13:06:49 +0200

    Update EdgeStreamTest.java

--- src/test/java/edu/leipzig/grafs/model/EdgeStreamTest.java
+++ src/test/java/edu/leipzig/grafs/model/EdgeStreamTest.java
@@ -1,9 +1,5 @@
 package edu.leipzig.grafs.model;
 
-import edu.leipzig.grafs.operators.grouping.Grouping;
-import edu.leipzig.grafs.operators.grouping.functions.PropertiesAggregationFunction;
-import edu.leipzig.grafs.operators.grouping.model.AggregationMapping;
-import edu.leipzig.grafs.operators.grouping.model.GroupingInformation;
 import edu.leipzig.grafs.util.AsciiGraphLoader;
 import edu.leipzig.grafs.util.FlinkConfig;
 import edu.leipzig.grafs.util.FlinkConfigBuilder;
@@ -11,10 +7,6 @@ import java.time.Duration;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
-import org.apache.flink.streaming.api.windowing.time.Time;
-import org.gradoop.common.model.impl.properties.PropertyValue;
-import org.junit.jupiter.api.Test;
 
 class EdgeStreamTest {
 
@@ -45,33 +37,5 @@ class EdgeStreamTest {
             .withTimestampAssigner((ec, timestamp) -> 0))
         .build();
     edgeStream = loader.createEdgeStream(config);
-  }
-
-  @Test
-  void groupBy() throws Exception {
-    GroupingInformation vertexEgi = new GroupingInformation();
-    vertexEgi.addKey("n");
-    AggregationMapping am = new AggregationMapping();
-    var identity = new PropertyValue();
-    identity.setInt(0);
-    am.addAggregationForProperty("a",
-        new PropertiesAggregationFunction("a", identity, (PropertyValue pV1, PropertyValue pV2) -> {
-          var newVal = new PropertyValue();
-          newVal.setInt(pV1.getInt() + pV2.getInt());
-          return newVal;
-        }));
-    edgeStream.callForStream(
-        Grouping.createGrouping()
-            .addVertexGroupingKey("n")
-            .addVertexAggregateFunction("a",
-                new PropertiesAggregationFunction("a", identity,
-                    (PropertyValue pV1, PropertyValue pV2) -> {
-                      var newVal = new PropertyValue();
-                      newVal.setInt(pV1.getInt() + pV2.getInt());
-                      return newVal;
-                    }))
-            .buildWithWindow(TumblingEventTimeWindows.of(Time.milliseconds(10))))
-        .print();
-    env.execute();
   }
 }
