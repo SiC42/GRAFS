9e7ed16a1289f4ce59873da7774c5b2e6404d4df
Simon Bordewisch <sbordewisch@gmx.de>
2020-09-25 14:32:15 +0200

    Add serialization to benchmark subgraph

--- src/main/java/edu/leipzig/grafs/benchmarking/subgraph/BenchmarkSubgraph.java
+++ src/main/java/edu/leipzig/grafs/benchmarking/subgraph/BenchmarkSubgraph.java
@@ -5,9 +5,15 @@ import edu.leipzig.grafs.model.Edge;
 import edu.leipzig.grafs.model.EdgeContainer;
 import edu.leipzig.grafs.model.Vertex;
 import edu.leipzig.grafs.operators.subgraph.Subgraph;
+import java.io.IOException;
+import java.io.Serializable;
 import org.apache.flink.api.common.functions.FilterFunction;
 
-public class BenchmarkSubgraph extends Subgraph {
+public class BenchmarkSubgraph extends Subgraph implements Serializable {
+
+  protected BenchmarkSubgraph() {
+    super();
+  }
 
   public BenchmarkSubgraph(
       FilterFunction<Vertex> vertexFilter,
@@ -22,11 +28,22 @@ public class BenchmarkSubgraph extends Subgraph {
       Strategy strategy,
       String meterName) {
     super(vertexFilter, edgeFilter, strategy);
-    this.ecFilter = new FilterFunctionWithMeter<EdgeContainer>(meterName) {
+    var tempFilter = ecFilter;
+    ecFilter = new FilterFunctionWithMeter<>(meterName) {
       @Override
       public boolean plainFilter(EdgeContainer edgeContainer) throws Exception {
-        return ecFilter.filter(edgeContainer);
+        return tempFilter.filter(edgeContainer);
       }
     };
+  }
+
+  private void writeObject(java.io.ObjectOutputStream out)
+      throws IOException {
+    out.writeObject(ecFilter);
+  }
+
+  @SuppressWarnings("unchecked")
+  private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
+    this.ecFilter = (FilterFunction<EdgeContainer>) in.readObject();
   }
 }
