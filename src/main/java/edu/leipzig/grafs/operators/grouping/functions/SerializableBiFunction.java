f62a02b00124f260341cf6fda7f8dc6b4aee158f
Simon Bordewisch <sbordewisch@gmx.de>
2020-09-25 13:12:59 +0200

    Delete SerializableBiFunction.java

--- src/main/java/edu/leipzig/grafs/operators/grouping/functions/SerializableBiFunction.java
+++
@@ -1,8 +0,0 @@
-package edu.leipzig.grafs.operators.grouping.functions;
-
-import java.io.Serializable;
-import java.util.function.BiFunction;
-
-public interface SerializableBiFunction<S, T, R> extends BiFunction<S, T, R>, Serializable {
-
-}
