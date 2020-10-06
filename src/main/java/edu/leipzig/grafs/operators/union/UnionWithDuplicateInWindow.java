9d20dbb17a72813b605535d250465c9bedba1c0e
Simon Bordewisch <sbordewisch@gmx.de>
2020-10-06 14:34:26 +0200

    Add trigger to UnionWithDuplicateInWindow

--- src/main/java/edu/leipzig/grafs/operators/union/UnionWithDuplicateInWindow.java
+++ src/main/java/edu/leipzig/grafs/operators/union/UnionWithDuplicateInWindow.java
@@ -7,6 +7,7 @@ import edu.leipzig.grafs.operators.interfaces.GraphToGraphOperatorI;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
 import org.apache.flink.streaming.api.windowing.assigners.WindowAssigner;
+import org.apache.flink.streaming.api.windowing.triggers.Trigger;
 import org.apache.flink.streaming.api.windowing.windows.Window;
 import org.apache.flink.util.Collector;
 
@@ -15,10 +16,16 @@ public class UnionWithDuplicateInWindow<W extends Window> implements
 
   private final EdgeStream[] streams;
   private final WindowAssigner<Object, W> window;
+  private final Trigger<EdgeContainer, W> trigger;
 
   public UnionWithDuplicateInWindow(WindowAssigner<Object, W> window, EdgeStream... streams) {
+    this(window, null, streams);
+  }
 
+  public UnionWithDuplicateInWindow(WindowAssigner<Object, W> window,
+      Trigger<EdgeContainer, W> trigger, EdgeStream... streams) {
     this.streams = streams;
+    this.trigger = trigger;
     this.window = window;
   }
 
@@ -36,6 +43,7 @@ public class UnionWithDuplicateInWindow<W extends Window> implements
     return unionedStream
         .keyBy(EdgeContainer::toString)
         .window(window)
+        .trigger(trigger)
         .process(filterDuplicateInWindowFunction);
 
   }
