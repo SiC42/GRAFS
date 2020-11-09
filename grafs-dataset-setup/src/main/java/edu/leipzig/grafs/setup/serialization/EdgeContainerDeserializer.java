4dbc2a2a3e64b80e63c52b3859be470f8790b8b6
Simon Bordewisch <sbordewisch@gmx.de>
2020-11-09 13:07:22 +0100

    Create EdgeContainerDeserializer.java

---
+++ grafs-dataset-setup/src/main/java/edu/leipzig/grafs/setup/serialization/EdgeContainerDeserializer.java
@@ -0,0 +1,25 @@
+package edu.leipzig.grafs.setup.serialization;
+
+
+import edu.leipzig.grafs.model.EdgeContainer;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import org.apache.kafka.common.serialization.Deserializer;
+
+public class EdgeContainerDeserializer implements Deserializer<EdgeContainer>{
+
+  @Override
+  public EdgeContainer deserialize(String s, byte[] bytes) {
+    try {
+      var bais = new ByteArrayInputStream(bytes);
+      var ois = new ObjectInputStream(bais);
+      var ec = (EdgeContainer) ois.readObject();
+      ois.close();
+      return ec;
+    } catch (IOException | ClassNotFoundException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+}
