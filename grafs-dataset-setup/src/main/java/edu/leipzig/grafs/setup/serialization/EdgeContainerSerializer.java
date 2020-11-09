c2fe1a1ef7721ca6609c8136dcc9eced24130147
Simon Bordewisch <sbordewisch@gmx.de>
2020-11-09 13:07:32 +0100

    Update EdgeContainerSerializer.java

--- grafs-dataset-setup/src/main/java/edu/leipzig/grafs/setup/serialization/EdgeContainerSerializer.java
+++ grafs-dataset-setup/src/main/java/edu/leipzig/grafs/setup/serialization/EdgeContainerSerializer.java
@@ -4,10 +4,13 @@ import edu.leipzig.grafs.model.EdgeContainer;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import org.apache.kafka.common.serialization.Serializer;
 
-public class EdgeContainerSerializer {
+public class EdgeContainerSerializer implements
+    Serializer<EdgeContainer> {
 
-  public byte[] serialize(EdgeContainer ec) {
+  @Override
+  public byte[] serialize(String s, EdgeContainer ec) {
     try {
       var baos = new ByteArrayOutputStream();
       var oos = new ObjectOutputStream(baos);
@@ -19,5 +22,4 @@ public class EdgeContainerSerializer {
       return null;
     }
   }
-
 }
