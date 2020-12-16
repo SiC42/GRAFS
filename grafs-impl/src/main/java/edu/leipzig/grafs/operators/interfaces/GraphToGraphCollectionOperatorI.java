879970e5ba977bf28d842a74c404197f4faaae55
Simon Bordewisch <sbordewisch@gmx.de>
2020-12-16 12:49:28 +0100

    Add javadoc to the G2GC & G2G operator interfaces

--- grafs-impl/src/main/java/edu/leipzig/grafs/operators/interfaces/GraphToGraphCollectionOperatorI.java
+++ grafs-impl/src/main/java/edu/leipzig/grafs/operators/interfaces/GraphToGraphCollectionOperatorI.java
@@ -1,5 +1,9 @@
 package edu.leipzig.grafs.operators.interfaces;
 
+/**
+ * Interface for operators which takes a graph and produces multiple graphs (i.e. graph collections).
+ * This is only a formal definition and not used in any other way than to provide a visual indicator which operators do what.
+ */
 public interface GraphToGraphCollectionOperatorI extends OperatorI {
 
 }
