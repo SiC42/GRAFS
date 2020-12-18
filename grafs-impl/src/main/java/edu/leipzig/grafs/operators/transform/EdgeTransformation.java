be0cdc2c89276c7418c51a5713c0fd366282090e
Simon Bordewisch <sbordewisch@gmx.de>
2020-12-18 13:34:48 +0100

    Add javadoc to transformation operators

--- grafs-impl/src/main/java/edu/leipzig/grafs/operators/transform/EdgeTransformation.java
+++ grafs-impl/src/main/java/edu/leipzig/grafs/operators/transform/EdgeTransformation.java
@@ -6,10 +6,20 @@ import edu.leipzig.grafs.operators.interfaces.GraphToGraphOperatorI;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
 
+/**
+ * Represents an Edge Transformation Operator. The given transformation function is applied to all edges in the stream.
+ */
 public class EdgeTransformation implements GraphToGraphOperatorI {
 
+  /**
+   * Function that is applied to the stream
+   */
   protected MapFunction<EdgeContainer, EdgeContainer> ecMapper;
 
+  /**
+   * Initializes this operator with the given transformation function.
+   * @param mapper transformation function that is used on every edge of the stream
+   */
   public EdgeTransformation(final MapFunction<Edge, Edge> mapper) {
     this.ecMapper = ec -> {
       var transformedEdge = mapper.map(ec.getEdge());
@@ -17,6 +27,11 @@ public class EdgeTransformation implements GraphToGraphOperatorI {
     };
   }
 
+  /**
+   * Applies this operator on the stream and returns the stream with the operator applied.
+   * @param stream stream on which the operator should be applied
+   * @return the stream with the edge transformation operator applied
+   */
   @Override
   public DataStream<EdgeContainer> execute(DataStream<EdgeContainer> stream) {
     return stream.map(ecMapper);
--- grafs-impl/src/main/java/edu/leipzig/grafs/operators/transform/VertexTransformation.java
+++ grafs-impl/src/main/java/edu/leipzig/grafs/operators/transform/VertexTransformation.java
@@ -6,13 +6,20 @@ import edu.leipzig.grafs.operators.interfaces.GraphToGraphOperatorI;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
 
+/**
+ * Represents a Vertex Transformation Operator. The given transformation function is applied to all vertices in the stream.
+ */
 public class VertexTransformation implements GraphToGraphOperatorI {
 
+  /**
+   * Function that is applied to the stream
+   */
   protected MapFunction<EdgeContainer, EdgeContainer> ecMapper;
 
-  protected VertexTransformation() {
-  }
-
+  /**
+   * Initializes this operator with the given transformation function.
+   * @param mapper transformation function that is used on every vertice of the stream
+   */
   public VertexTransformation(final MapFunction<Vertex, Vertex> mapper) {
     this.ecMapper = ec -> {
       Vertex source = mapper.map(ec.getSourceVertex());
@@ -21,6 +28,11 @@ public class VertexTransformation implements GraphToGraphOperatorI {
     };
   }
 
+  /**
+   * Applies this operator on the stream and returns the stream with the operator applied.
+   * @param stream stream on which the operator should be applied
+   * @return the stream with the vertex transformation operator applied
+   */
   @Override
   public DataStream<EdgeContainer> execute(DataStream<EdgeContainer> stream) {
     return stream.map(ecMapper);
