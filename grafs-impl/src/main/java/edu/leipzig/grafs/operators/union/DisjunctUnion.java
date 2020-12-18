9983981e0a3eb80a8341d35efa2890b3f52b9ea5
Simon Bordewisch <sbordewisch@gmx.de>
2020-12-18 13:45:57 +0100

    Add javadoc to union

--- grafs-impl/src/main/java/edu/leipzig/grafs/operators/union/DisjunctUnion.java
+++ grafs-impl/src/main/java/edu/leipzig/grafs/operators/union/DisjunctUnion.java
@@ -6,14 +6,32 @@ import edu.leipzig.grafs.operators.interfaces.GraphToGraphCollectionOperatorI;
 import edu.leipzig.grafs.operators.interfaces.GraphToGraphOperatorI;
 import org.apache.flink.streaming.api.datastream.DataStream;
 
+/**
+ * This operator unifies multiple streams into the given one. It is implied, that the set of
+ * elements in both streams are disjunct (or it is not of importance for the analysis.
+ */
 public class DisjunctUnion implements GraphToGraphOperatorI, GraphToGraphCollectionOperatorI {
 
+  /**
+   * Streams that should be unified via the union operation.
+   */
   private final EdgeStream[] streams;
 
+  /**
+   * Initializes this operator with the given streams.
+   *
+   * @param streams streams that should be unified by applying the union operator onto a stream
+   */
   public DisjunctUnion(EdgeStream... streams) {
     this.streams = streams;
   }
 
+  /**
+   * Applies the streams to this stream and returns the unified stream
+   *
+   * @param stream stream on which the operator should be applied
+   * @return unified stream
+   */
   @Override
   public DataStream<EdgeContainer> execute(DataStream<EdgeContainer> stream) {
     var dataStreams = new DataStream[streams.length];
--- grafs-impl/src/main/java/edu/leipzig/grafs/operators/union/UnionWithDuplicateInWindow.java
+++ grafs-impl/src/main/java/edu/leipzig/grafs/operators/union/UnionWithDuplicateInWindow.java
@@ -11,17 +11,45 @@ import org.apache.flink.streaming.api.windowing.triggers.Trigger;
 import org.apache.flink.streaming.api.windowing.windows.Window;
 import org.apache.flink.util.Collector;
 
+/**
+ * This operator unifies multiple streams into the given one. Duplicate elements are detected in a
+ * window and only one is returned. It is advised to only use Tumbling Windows.
+ */
 public class UnionWithDuplicateInWindow<W extends Window> implements
     GraphToGraphOperatorI, GraphToGraphCollectionOperatorI {
 
+  /**
+   * Streams that should be unified via the union operation.
+   */
   private final EdgeStream[] streams;
+
+  /**
+   * Window in which only distinct elements should exist
+   */
   private final WindowAssigner<Object, W> window;
+
+  /**
+   * Optional trigger
+   */
   private final Trigger<EdgeContainer, W> trigger;
 
+  /**
+   * Initializes the operator with the given parameters.
+   *
+   * @param window  window in which only distinct elements should exist
+   * @param streams streams that should be unified by applying the union operator onto a stream
+   */
   public UnionWithDuplicateInWindow(WindowAssigner<Object, W> window, EdgeStream... streams) {
     this(window, null, streams);
   }
 
+  /**
+   * Initializes the operator with the given parameters.
+   *
+   * @param window  window in which only distinct elements should exist
+   * @param streams streams that should be unified by applying the union operator onto a stream
+   * @param trigger optional window trigger that is used for this operation
+   */
   public UnionWithDuplicateInWindow(WindowAssigner<Object, W> window,
       Trigger<EdgeContainer, W> trigger, EdgeStream... streams) {
     this.streams = streams;
@@ -29,6 +57,13 @@ public class UnionWithDuplicateInWindow<W extends Window> implements
     this.window = window;
   }
 
+  /**
+   * Applies the streams to this stream and returns the unified stream with distinct elements in the
+   * given window.
+   *
+   * @param stream stream on which the operator should be applied
+   * @return unified stream
+   */
   @Override
   public DataStream<EdgeContainer> execute(DataStream<EdgeContainer> stream) {
     var unionedStream = new DisjunctUnion(streams).execute(stream);
@@ -41,7 +76,7 @@ public class UnionWithDuplicateInWindow<W extends Window> implements
           }
         };
     return unionedStream
-        .keyBy(EdgeContainer::toString)
+        .keyBy(ec -> ec.getEdge().getId().toString())
         .window(window)
         .trigger(trigger)
         .process(filterDuplicateInWindowFunction);
