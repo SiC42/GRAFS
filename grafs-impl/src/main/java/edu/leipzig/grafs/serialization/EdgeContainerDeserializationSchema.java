c148a5791b64c1b98f9fd9f570d9e5e799bd82c2
Simon Bordewisch <sbordewisch@gmx.de>
2021-01-06 14:53:22 +0100

    Add javadoc for serialization classes

--- grafs-impl/src/main/java/edu/leipzig/grafs/serialization/EdgeContainerDeserializationSchema.java
+++ grafs-impl/src/main/java/edu/leipzig/grafs/serialization/EdgeContainerDeserializationSchema.java
@@ -8,10 +8,22 @@ import java.io.ObjectInputStream;
 import org.apache.flink.api.common.serialization.DeserializationSchema;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 
+/**
+ * Allows Deserialization of Edge Containers in Kafka Sources.
+ */
 public class EdgeContainerDeserializationSchema implements DeserializationSchema<EdgeContainer> {
 
+  /**
+   * Acts as the indicator for Flink to end the stream, as all elements have been processed.
+   */
   public static final String END_OF_STREAM_LABEL = "_endOfStream";
 
+  /**
+   * Deserialzes an array of bytes into an edge container and returns it.
+   *
+   * @param bytes bytes representation of an edge container object
+   * @return object representation of given bytes
+   */
   @Override
   public EdgeContainer deserialize(byte[] bytes) {
     try {
@@ -26,12 +38,26 @@ public class EdgeContainerDeserializationSchema implements DeserializationSchema
     }
   }
 
+  /**
+   * Returns <tt>true</tt> if a end of stream element is found and thus ends the stream.
+   * <p>
+   * The edge in the edge container needs to have the END_OF_STREAM_LABEL. * @param edgeContainer
+   * element which should be checked if it is an end of stream element
+   *
+   * @return <tt>true</tt> if the given element contains the END_OF_STREAM_LABEL, <tt>false</tt>
+   * otherwise
+   */
   @Override
   public boolean isEndOfStream(EdgeContainer edgeContainer) {
     var edge = edgeContainer.getEdge();
     return edgeContainer.getEdge().getLabel().equals(END_OF_STREAM_LABEL);
   }
 
+  /**
+   * Returns type information of the elements produced by this class.
+   *
+   * @return type information of the elements produced by this class
+   */
   @Override
   public TypeInformation<EdgeContainer> getProducedType() {
     return TypeInformation.of(EdgeContainer.class);
--- grafs-impl/src/main/java/edu/leipzig/grafs/serialization/EdgeContainerSerializer.java
+++ grafs-impl/src/main/java/edu/leipzig/grafs/serialization/EdgeContainerSerializer.java
@@ -7,9 +7,19 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 import org.apache.kafka.common.serialization.Serializer;
 
+/**
+ * Serializer used to serialize edge container into a byte array.
+ */
 public class EdgeContainerSerializer implements Serializer<EdgeContainer> {
 
 
+  /**
+   * Serializes the given edge container into a byte array and returns it
+   *
+   * @param s  ignored
+   * @param ec edge container which should be serialized into byte representation
+   * @return byte representation of given edge container
+   */
   @Override
   public byte[] serialize(String s, EdgeContainer ec) {
     try {
