e1910519b88e34f1aaa77cab11f18af15f9b9c8b
Simon Bordewisch <sbordewisch@gmx.de>
2021-01-27 15:50:26 +0100

    Add manual tests

---
+++ grafs-dataset-setup/src/test/java/manual/FileConsumer.java
@@ -0,0 +1,34 @@
+package manual;
+
+import edu.leipzig.grafs.model.Triplet;
+import edu.leipzig.grafs.serialization.TripletDeserializationSchema;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+/**
+ * Simple tests class, reads the given serialized edge container file and prints the last 10 ECs.
+ */
+public class FileConsumer {
+
+  public static void main(String[] args) throws IOException, ClassNotFoundException {
+    var fileOutputStream = new FileInputStream("resources/Triplet_10.serialized");
+    var ois = new ObjectInputStream(fileOutputStream);
+    var triplet = (Triplet) ois.readObject();
+    int counter = 1;
+    var queue = new SmallQueue();
+    while (!triplet.getEdge().getLabel()
+        .equals(TripletDeserializationSchema.END_OF_STREAM_LABEL)) {
+      queue.add(triplet);
+      counter++;
+      if (counter % 10000 == 0) {
+        System.out.print(counter + "\r");
+        System.out.flush();
+      }
+      triplet = (Triplet) ois.readObject();
+    }
+    queue.add(triplet);
+    queue.asColl().forEach(System.out::println);
+  }
+
+}
---
+++ grafs-dataset-setup/src/test/java/manual/SmallQueue.java
@@ -0,0 +1,25 @@
+package manual;
+
+import edu.leipzig.grafs.model.Triplet;
+import java.util.LinkedList;
+import java.util.List;
+
+public class SmallQueue {
+
+  private final LinkedList<Triplet> queue;
+
+  public SmallQueue() {
+    queue = new LinkedList<>();
+  }
+
+  public void add(Triplet triplet) {
+    if (queue.size() >= 10) {
+      queue.removeFirst();
+    }
+    queue.add(triplet);
+  }
+
+  public List<Triplet> asColl() {
+    return queue;
+  }
+}
