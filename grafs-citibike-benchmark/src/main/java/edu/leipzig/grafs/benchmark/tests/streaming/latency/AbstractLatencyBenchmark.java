ca98b2313541cf7bac5f4a9d9f08dc6596058338
Simon Bordewisch <sbordewisch@gmx.de>
2021-02-10 14:28:32 +0100

    Fix issues with wrong pipeline

--- grafs-citibike-benchmark/src/main/java/edu/leipzig/grafs/benchmark/tests/streaming/latency/AbstractLatencyBenchmark.java
+++ grafs-citibike-benchmark/src/main/java/edu/leipzig/grafs/benchmark/tests/streaming/latency/AbstractLatencyBenchmark.java
@@ -1,8 +1,13 @@
 package edu.leipzig.grafs.benchmark.tests.streaming.latency;
 
 import edu.leipzig.grafs.benchmark.tests.streaming.AbstractStreamingBenchmark;
+import edu.leipzig.grafs.model.Edge;
+import edu.leipzig.grafs.model.Triplet;
+import edu.leipzig.grafs.model.Vertex;
+import edu.leipzig.grafs.model.streaming.GraphStream;
 import edu.leipzig.grafs.operators.transform.EdgeTransformation;
 import java.util.concurrent.TimeUnit;
+import org.apache.flink.api.common.functions.MapFunction;
 
 public abstract class AbstractLatencyBenchmark extends AbstractStreamingBenchmark {
 
@@ -14,19 +19,27 @@ public abstract class AbstractLatencyBenchmark extends AbstractStreamingBenchmar
   @Override
   public void execute() throws Exception {
     var timestampKey = "timestamp";
-    var baseStream = this.stream
-        .callForGraph(new EdgeTransformation(e -> {
-          e.setProperty(timestampKey, System.currentTimeMillis());
-          return e;
-        }));
-    var graphStream = applyOperator(baseStream);
-    var datastream = stream.getDataStream();
-    datastream.map(t -> {
-      var startTime = t.getEdge().getPropertyValue(timestampKey).getLong();
-      return System.currentTimeMillis() - startTime;
-    });
+    var dataStream = this.stream.getDataStream();
+    dataStream = dataStream.map(new MapFunction<Triplet<Vertex, Edge>, Triplet<Vertex, Edge>>() {
+
+      @Override
+      public Triplet<Vertex, Edge> map(Triplet<Vertex, Edge> triplet) throws Exception {
+        triplet.getEdge().setProperty(timestampKey, System.currentTimeMillis());
+        return triplet;
+      }
+    }).name("Set Timestamp").setParallelism(96);
+    var graphStream = applyOperator(new GraphStream(dataStream, config));
+    var afterOpStream = graphStream.getDataStream();
+    var longStream = afterOpStream.map(new MapFunction<Triplet<Vertex, Edge>, Long>() {
+
+      @Override
+      public Long map(Triplet<Vertex, Edge> triplet) throws Exception {
+          var startTime = triplet.getEdge().getPropertyValue(timestampKey).getLong();
+          return System.currentTimeMillis() - startTime;
+      }
+    }).setParallelism(96).name("Extracting Latency");
     var path = properties.getProperty(OUTPUT_PATH_KEY);
-    datastream.writeAsText(path);
+    longStream.writeAsText(path).setParallelism(96).name("Write to file sink");
     var result = env.execute(properties.getProperty(OPERATOR_NAME_KEY));
     var timeInMilliSeconds = result.getNetRuntime(TimeUnit.MILLISECONDS);
     outputWriter.write(getCsvLine(timeInMilliSeconds));
