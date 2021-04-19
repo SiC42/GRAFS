b295fbbae873a3c23fe9b956cd8a82f661aafa60
Simon Bordewisch <sbordewisch@gmx.de>
2021-04-19 14:09:58 +0200

    Add Generic type to Triplet deserialization

--- grafs-dataset-setup/src/main/java/edu/leipzig/grafs/setup/serialization/TripletDeserializer.java
+++ grafs-dataset-setup/src/main/java/edu/leipzig/grafs/setup/serialization/TripletDeserializer.java
@@ -1,20 +1,22 @@
 package edu.leipzig.grafs.setup.serialization;
 
 
+import edu.leipzig.grafs.model.Edge;
 import edu.leipzig.grafs.model.Triplet;
+import edu.leipzig.grafs.model.Vertex;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import org.apache.kafka.common.serialization.Deserializer;
 
-public class TripletDeserializer implements Deserializer<Triplet> {
+public class TripletDeserializer implements Deserializer<Triplet<Vertex, Edge>> {
 
   @Override
-  public Triplet deserialize(String s, byte[] bytes) {
+  public Triplet<Vertex, Edge> deserialize(String s, byte[] bytes) {
     try {
       var bais = new ByteArrayInputStream(bytes);
       var ois = new ObjectInputStream(bais);
-      var triplet = (Triplet) ois.readObject();
+      var triplet = (Triplet<Vertex, Edge>) ois.readObject();
       ois.close();
       return triplet;
     } catch (IOException | ClassNotFoundException e) {
